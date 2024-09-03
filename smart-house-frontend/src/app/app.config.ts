560bdbc79f6771df339a7866a7bfd263afd780bd
lukastan1999 <lukastan1999@gmail.com>
2024-09-03 18:10:39 +0200

    registracija front (ne radi)

--- smart-house-frontend/src/app/app.config.ts
+++ smart-house-frontend/src/app/app.config.ts
@@ -1,8 +1,34 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+// import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+// import { provideRouter } from '@angular/router';
 
-import { routes } from './app.routes';
+// import { routes } from './app.routes';
 
-export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
-};
+// export const appConfig: ApplicationConfig = {
+//   providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+// };
+
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
+
+import { AppComponent } from './app.component';
+import { RegistrationComponent } from './registration/registration.component';
+
+@NgModule({
+  declarations: [
+    RegistrationComponent
+  ],
+  imports: [
+    BrowserModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    RouterModule.forRoot([
+      { path: 'register', component: RegistrationComponent },
+      { path: '', redirectTo: '/register', pathMatch: 'full' }
+    ])
+  ],
+  providers: []
+})
+export class AppModule { }
--- smart-house-frontend/src/app/registration/registration.component.ts
+++ smart-house-frontend/src/app/registration/registration.component.ts
@@ -1,12 +1,52 @@
+// import { Component } from '@angular/core';
+
+// @Component({
+//   selector: 'app-registration',
+//   standalone: true,
+//   imports: [],
+//   templateUrl: './registration.component.html',
+//   styleUrl: './registration.component.css'
+// })
+// export class RegistrationComponent {
+
+// }
+
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-registration',
-  standalone: true,
-  imports: [],
   templateUrl: './registration.component.html',
-  styleUrl: './registration.component.css'
+  styleUrls: ['./registration.component.css']
 })
 export class RegistrationComponent {
+  registrationForm: FormGroup;
+  errorMessage: string | null = null;
 
+  constructor(private fb: FormBuilder, private http: HttpClient, private router: Router) {
+    this.registrationForm = this.fb.group({
+      name: ['', Validators.required],
+      surname: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required],
+      confirmPassword: ['', Validators.required],
+      address: ['', Validators.required],
+      phoneNumber: ['', Validators.required],
+      role: ['GUEST', Validators.required],  // Default to GUEST, can be changed in the form
+    });
+  }
+
+  onSubmit() {
+    if (this.registrationForm.valid) {
+      const registrationData = this.registrationForm.value;
+      this.http.post('http://localhost:8080/api/register', registrationData)
+        .subscribe(response => {
+          console.log('Registration successful', response);
+        }, error => {
+          console.error('Registration failed', error);
+        });
+    }
+  }
 }
