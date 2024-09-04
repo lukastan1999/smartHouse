c1c06b0afc385e6e4aaf1d75e9a55435114adbdc
lukastan1999 <lukastan1999@gmail.com>
2024-09-04 23:42:26 +0200

    Generisana komponenta logina na FRONTU

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -6,13 +6,15 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RegistrationComponent } from './components/registration/registration.component';
+import { LoginComponent } from './components/login/login.component';
 
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    RegistrationComponent
+    RegistrationComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
